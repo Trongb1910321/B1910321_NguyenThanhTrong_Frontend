bb0f3dff52bb40cc2518cd75f8acbe0c75e82715
Trongb1910321 <114137576+Trongb1910321@users.noreply.github.com>
2023-01-16 16:41:56 +0700

    Cai dat trang thai loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+        <div class="page">
+            <p>
+                Oops, Không thể tìm thấy trang. Trở về
+                <router-link to="/">trang chủ.</router-link>
+            </p>
+        </div>
+
+</template>
